eb961cf48cd9eea32c9984741b7fc6ff72b3660d
UserEdd <eduardodominguez@laselva.edu.mx>
2024-06-12 11:40:42 -0600

    Operaciones

---
+++ operaciones.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suma y resta</title>
+</head>
+<body>
+    <h1>Eduardo Antonio Domínguez Santiago</h1>
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        
+        <label for="numero1">Numero 1:</label>
+        <input type="number" id="numero1" name="numero1" required>
+
+        <label for="numero2">Numero 2:</label>
+        <input type="number" id="numero2" name="numero2" required>
+
+        <br>
+
+        <input type="submit" name="operacion" value="Sumar">
+        <input type="submit" name="operacion" value="Restar">
+
+        <br>
+
+        <?php
+            if (isset($_POST['numero1']) && isset($_POST['numero2']) && isset($_POST['operacion'])) {
+                $numero1 = $_POST['numero1'];
+                $numero2 = $_POST['numero2'];
+                $operacion = $_POST['operacion'];
+
+                $resultado = 0;
+
+                if ($operacion == "Sumar") {
+                    $resultado = $numero1 + $numero2;
+                } else if ($operacion == "Restar") {
+                    $resultado = $numero1 - $numero2;
+                }
+
+                echo "<p>Resultado: $resultado</p>";
+            }
+        ?>
+    </form>
+</body>
+</html>
